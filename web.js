1ea85e61e7de00723c65ce91da67aaf868a9142c
Victor Barriga <victorbarriga@live.com>
2013-07-27 01:21:21 +0000

    Modified web.js added language

--- web.js
+++ web.js
@@ -1,1 +1,14 @@
-test
+var express = require('express');
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+    var fs = require('fs');
+    var content = fs.readFileSync('index.html', 'utf-8');
+    response.send(content);
+
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
